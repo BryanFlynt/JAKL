7c54fac2125468f60ab2d30712614eb6fb095157
Bryan Flynt <bflynt@gravity.hsd1.co.comcast.net>
2020-10-15 00:27:55 -0600

    Initial Layout

---
+++ include/jakl/core/context.hpp
@@ -0,0 +1,55 @@
+/*
+ * context.hpp
+ *
+ *  Created on: Oct 14, 2020
+ *      Author: bflynt
+ */
+
+#ifndef INCLUDE_JAKL_CORE_CONTEXT_HPP_
+#define INCLUDE_JAKL_CORE_CONTEXT_HPP_
+
+
+#include "jakl/core/device.hpp"
+
+
+namespace jakl {
+
+
+class Context final {
+
+public:
+	Context(Context const& other)            = default;
+	Context(Context&& other)                 = default;
+	~Context()                               = default;
+	Context& operator=(Context const& other) = default;
+	Context& operator=(Context&& other)      = default;
+
+	/** Create Context with Device
+	 */
+	Context(Device const& device) : device_(device){
+	}
+
+	/** Check if Context are the same
+	 */
+	bool operator==(Context const& other) const {
+		return device_ == other.device_;
+	}
+
+	/** Get Contexts device
+	 */
+	Device const& device() const noexcept {
+		return device_;
+	}
+
+
+private:
+	Device device_;
+};
+
+
+} // namespace jakl
+
+
+
+
+#endif /* INCLUDE_JAKL_CORE_CONTEXT_HPP_ */
